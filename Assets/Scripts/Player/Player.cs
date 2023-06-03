200e1027c8393fd169a8685daf2db709c6e26b82
Tyler <Tyler.lazarus01@outlook.com>
2023-06-03 16:49:18 +0100

    Commit 4 - Added Player forward facing collision

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,7 +6,26 @@ public class Player : MonoBehaviour
 {
     private void Update()
     {
+        ForwardCheck();
+    }
 
+    private void ForwardCheck()
+    {
+        Debug.DrawRay(transform.position + new Vector3(0, 0.1f, 0), gameObject.transform.forward * 0.25f, Color.red);
+        if (Physics.Raycast(transform.position + new Vector3(0, 0.1f, 0), gameObject.transform.forward, out RaycastHit frontHit, 0.25f))
+        {
+            //if hits anything, end game
+        }
+    }
+
+    private void Jump()
+    {
+        Debug.DrawRay(transform.position + new Vector3(0, 0.1f, 0), -gameObject.transform.up * 0.25f, Color.red);
+        if (Physics.Raycast(gameObject.transform.position + new Vector3(0, 0.1f, 0), -gameObject.transform.up, out RaycastHit downHit, 0.25f))
+        {
+            //If hits floor, put character to it
+            //if hits anything else, end game.
+        }
     }
 
 }
